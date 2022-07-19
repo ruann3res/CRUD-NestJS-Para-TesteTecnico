bb5411db571a63df9fe65d8cf55745fe427dfba5
RuanN3res <ruan.neres@mblabs.com.br>
2022-07-19 18:03:37 -0300

    feat/Authentication Modules started

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CarsModule } from './cars/cars.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, CarsModule],
+  imports: [ConfigModule.forRoot(), UsersModule, CarsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
