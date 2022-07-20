0555f1c600b4ce5d205c0c5482756c9db310ce13
RuanN3res <ruan.neres@mblabs.com.br>
2022-07-19 21:15:42 -0300

    feat/Implement local strategy validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UsersService) {}
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPassowrdValid = await bcrypt.compare(password, user.password);
+
+      if (isPassowrdValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+      throw new Error('Email adress or password is incorrect');
+    }
+  }
+}
